407f6e3ee67379260a042fa7786b3b37c2fa8369
H61IB5G6O7GUYYB\Administrator <1207173433@qq.com>
2023-04-09 14:29:40 +0800

    Init

---
+++ Source/Invoker/InvokerGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InvokerGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AInvokerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AInvokerGameMode();
+};
+
+
+
